366391eea9cd474974d25b29a5fd34b8564edc9a
hannah <97944856+themsgcentre@users.noreply.github.com>
2026-02-04 13:47:34 +0100

    created project

---
+++ SkillHelper/src/main/java/com/example/skillhelper/SkillHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.skillhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillHelperApplication.class, args);
+    }
+
+}
---
+++ SkillHelper/src/test/java/com/example/skillhelper/SkillHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skillhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
