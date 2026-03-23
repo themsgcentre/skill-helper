340f20f875cb0fd92892e09552c29526c213fd4a
hannah <97944856+themsgcentre@users.noreply.github.com>
2026-03-23 12:14:00 +0100

    fixed json parse errors

--- SkillHelper/build.gradle.kts
+++ SkillHelper/build.gradle.kts
@@ -27,6 +27,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
